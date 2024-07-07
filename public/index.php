8bc391718587b1489fc4ffc4832ebe385b00506a
joandysson <joandysson.gama@gmail.com>
2024-07-07 12:34:03 -0300

    fix

--- public/index.php
+++ public/index.php
@@ -3,21 +3,6 @@
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Config' . DIRECTORY_SEPARATOR . 'functions.php';
 
-
-header("Referrer-Policy: no-referrer");
-
-// Specify domains from which requests are allowed
-header('Access-Control-Allow-Origin: *');
-
-// Specify which request methods are allowed
-header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
-
-// Additional headers which may be sent along with the CORS request
-header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
-
-// Set the age to 1 day to improve speed/caching.
-header('Access-Control-Max-Age: 86400');
-
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(dirname(__DIR__));
 $dotenv->load();
 
--- server.php
+++ server.php
@@ -1,5 +1,19 @@
 <?php
 
+header("Referrer-Policy: no-referrer");
+
+// Specify domains from which requests are allowed
+header('Access-Control-Allow-Origin: *');
+
+// Specify which request methods are allowed
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+
+// Additional headers which may be sent along with the CORS request
+header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
+
+// Set the age to 1 day to improve speed/caching.
+header('Access-Control-Max-Age: 86400');
+
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
