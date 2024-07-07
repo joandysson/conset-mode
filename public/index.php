4b7e8698189b7f0b954b5fbbfefda5641d8b7b20
joandysson <joandysson.gama@gmail.com>
2024-07-07 12:26:12 -0300

    cors

--- public/index.php
+++ public/index.php
@@ -3,10 +3,6 @@
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Config' . DIRECTORY_SEPARATOR . 'functions.php';
 
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(dirname(__DIR__));
 $dotenv->load();
 
