eae505925d527235e434757811fe30ffae5da575
joandysson <joandysson.gama@gmail.com>
2024-07-10 09:51:39 -0300

    fix

--- public/views/politics-privacy.php
+++ public/views/politics-privacy.php
@@ -34,31 +34,15 @@ $head = [
             <p><span style="color: rgb(68, 68, 68);">Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</span></p>
             <p><span style="color: rgb(68, 68, 68);">
                     <h2><span style="color: rgb(68, 68, 68);">Cookie Policy for toolz</span></h2>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <p><span style="color: rgb(68, 68, 68);">This is the Cookie Policy for toolz, accessible from URL https://toolz.at/.</span></p>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <h3><span style="color: rgb(68, 68, 68);">What Are Cookies</span></h3>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <p><span style="color: rgb(68, 68, 68);">As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or break certain elements of the sites functionality.</span></p>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <h3><span style="color: rgb(68, 68, 68);">How We Use Cookies</span></h3>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <p><span style="color: rgb(68, 68, 68);">We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</span></p>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <h3><span style="color: rgb(68, 68, 68);">Disabling Cookies</span></h3>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <p><span style="color: rgb(68, 68, 68);">You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of this site. Therefore it is recommended that you do not disable cookies.</span></p>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
-                    <h3><span style="color: rgb(68, 68, 68);">The Cookies We Set</span></h3>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
-                    <p><br></p>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <h3><span style="color: rgb(68, 68, 68);">Third Party Cookies</span></h3>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <p><span style="color: rgb(68, 68, 68);">In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.</span></p>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
-                    <p><br></p>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
                     <p><span style="color: rgb(68, 68, 68);">
                             <ul>
                                 <li><span style="color: rgb(68, 68, 68);">The Google AdSense service we use to serve advertising uses a DoubleClick cookie to serve more relevant ads across the web and limit the number of times that a given ad is shown to you.</span></li>
@@ -66,9 +50,8 @@ $head = [
                                 <li><span style="color: rgb(68, 68, 68);">We use adverts to offset the costs of running this site and provide funding for further development. The behavioural advertising cookies used by this site are designed to ensure that we provide you with the most relevant adverts where possible by anonymously tracking your interests and presenting similar things that may be of interest.</span></li>
                                 <li><span style="color: rgb(68, 68, 68);">Several partners advertise on our behalf and affiliate tracking cookies simply allow us to see if our customers have come to the site through one of our partner sites so that we can credit them appropriately and where applicable allow our affiliate partners to provide any bonus that they may provide you for making a purchase.</span></li>
                             </ul>
-                        </span></p>
-                    <p><span style="color: rgb(68, 68, 68);"> </span></p>
-                    <p><br></p>
+                        </span>
+                    </p>
                 </span></p>
             <h3><span style="color: rgb(68, 68, 68);">User's responsibilities</span></h3>
             <p><span style="color: rgb(68, 68, 68);">The user undertakes the responsibility to make appropriate use of the contents and information offered on the site with enunciative, but not imitative, behaviour:</span></p>
