98fc9930eb4bdf32afc6fb0f41ccbe1f6a547bed
joandysson <joandysson.gama@gmail.com>
2024-07-10 14:22:13 -0300

    fix install app

--- public/views/home.php
+++ public/views/home.php
@@ -179,7 +179,7 @@ $head = [
             <section class="call-to-action">
                 <h2>Discover Our App</h2>
                 <p>Experience the simplicity and efficiency of our PWA app for creating consent mode banners.</p>
-                <button onclick="window.location.href='app.html'">Try the App</button>
+                <button class="install-app">Try the App</button>
             </section>
 
             <section class="section-default">
