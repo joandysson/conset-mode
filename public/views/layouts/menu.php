cf602755d831a0b29736665960f96810f99dd82d
joandysson <joandysson.gama@gmail.com>
2025-03-06 16:55:24 -0300

    Add alert styles and update menu/footer links for multilingual support

--- public/views/layouts/menu.php
+++ public/views/layouts/menu.php
@@ -1,11 +1,11 @@
 <nav>
     <button class="menu-toggle" aria-label="Toggle menu">&#9776;</button> <!-- Botão do menu -->
     <ul class="menu">
-        <li><a href="/">Home</a></li>
-        <li><a href="/generator">Generator</a></li>
-        <li><a href="laws">Laws</a></li>
-        <li><a href="why-use">Why use?</a></li>
-        <li><a href="cookies">Cookies</a></li>
-        <li><a href="contact">Contact</a></li>
+        <li><a href="/<?php echo getPrefixLang() ?>"><?php echo __('Home') ?> </a></li>
+        <li><a href="/<?php echo getPrefixLang() ?>/generator"><?php echo __('Generator') ?></a></li>
+        <li><a href="/<?php echo getPrefixLang() ?>/laws"><?php echo __('Laws') ?></a></li>
+        <li><a href="/<?php echo getPrefixLang() ?>/why-use"><?php echo __('Why use?') ?></a></li>
+        <li><a href="/<?php echo getPrefixLang() ?>/cookies"><?php echo __('Cookies') ?></a></li>
+        <li><a href="/<?php echo getPrefixLang() ?>/contact"><?php echo __('Contact') ?></a></li>
     </ul>
 </nav>
