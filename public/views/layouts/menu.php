0ff5b2c5b6a787fc4f7ceeef68d7db402109e583
joandysson <joandysson.gama@gmail.com>
2025-03-07 13:29:03 -0300

    switch languege

--- public/views/layouts/menu.php
+++ public/views/layouts/menu.php
@@ -7,5 +7,38 @@
         <li><a href="/<?php echo getPrefixLang() ?>/why-use"><?php echo __('Why use?') ?></a></li>
         <li><a href="/<?php echo getPrefixLang() ?>/cookies"><?php echo __('Cookies') ?></a></li>
         <li><a href="/<?php echo getPrefixLang() ?>/contact"><?php echo __('Contact') ?></a></li>
+        <li>
+            <div class="dropdown">
+                <span>
+                    <?php
+                        switch(getPrefixLang()) {
+                            case 'en':
+                                echo '<img width="15px" src="' . asset('images/svg/us.svg') . '" alt="">';
+                                echo __('English');
+                                break;
+                            case 'pt':
+                                echo '<img width="15px" src="' . asset('images/svg/br.svg') . '" alt="">';
+                                echo __('Portuguese');
+                                break;
+                        }
+                    ?>
+                    <img width="15px" src="<?php echo asset('images/svg/down-2-svgrepo-com.svg') ?>" alt="">
+                </span>
+                <div class="dropdown-content">
+                    <?php if(getPrefixLang() !== 'en' ) {?>
+                    <a href="<?php echo changeLang('en') ?>">
+                        <img width="15px" src="<?php echo asset('images/svg/us.svg') ?>" alt="">
+                        <?php echo __('English') ?>
+                    </a>
+                    <?php } ?>
+                    <?php if(getPrefixLang() !== 'pt' ) {?>
+                    <a href="<?php echo changeLang('pt') ?>">
+                        <img width="15px" src="<?php echo asset('images/svg/br.svg') ?>" alt="">
+                        <?php echo __('Portuguese') ?>
+                    </a>
+                    <?php } ?>
+                </div>
+            </div>
+        </li>
     </ul>
 </nav>
